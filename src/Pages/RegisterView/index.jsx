7cb916dd8e3d16e18c390d6ec1b24b3e926b06d3
ZornStolz <1143875103@u.icesi.edu.co>
2022-11-10 00:28:41 -0500

    progress in register view

--- src/Pages/RegisterView/index.jsx
+++ src/Pages/RegisterView/index.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import { Button, Form, Input } from "antd";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import moment from "moment/moment";
+import { useRef } from "react";
+import db from "../../Config/firebase";
+import { dateFormat } from "../../Config/moment.format";
 
 const RegisterView = () => {
-  return <div>RegisterView</div>;
-};
+  const userId = useRef("");
+  const assistanceId = useRef("");
+  const currentDay = moment().format(dateFormat);
 
+  const assistanceRef = collection(db, "assistance");
+  const assistanceQuery = query(
+    assistanceRef,
+    where("userId", "==", userId.current),
+    where("day", "==", currentDay)
+  );
+
+  const getAssistance = async () => {
+    const subUser = onSnapshot(assistanceQuery, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        assistanceId.current = doc?.id;
+        console.log("entro");
+      });
+    });
+
+    await subUser();
+    console.log(assistanceId.current);
+  };
+
+  const onFinish = async (values) => {
+    userId.current = values.userId;
+    await getAssistance();
+    //actualizar
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="on"
+    >
+      <Form.Item
+        label="Ingrese el Id del usuario"
+        name="userId"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingrese un numero de identificacion",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Validar Asistencia
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 export default RegisterView;
